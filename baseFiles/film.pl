65752a2d4eb40cc76adcdddda7973768ace5960f
MLaurentys <matheus_t.laurentys@hotmail.com>
2019-11-22 15:24:39 -0300

    after meeting

---
+++ baseFiles/film.pl
@@ -0,0 +1,11 @@
+filme(american_beauty, 1999).
+diretor(american_beauty, sam_mendes).
+ator(american_beauty, kevin_spacey, lester_burnham).
+atriz(american_beauty, annette_bening, carolyn_burnham).
+atriz(american_beauty, thora_birch, jane_burnham).
+
+nome(sam_mendes, 'Sam', 'Mendes').
+nome(kevin_spacey, 'Kevin', 'Spacey').
+nome(annette_bening, 'Annette', 'Bening').
+nome(thora_birch, 'Thora', 'Birch').
+tit_dur(american_beauty, 'American Beauty', 122).
